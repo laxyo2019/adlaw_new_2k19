16d24793b2b8528cbadb41dd89226471b5c67325
ritesh845 <riteshpanchal845@gmail.com>
2020-05-15 14:16:01 +0530

    lawfirm page content change 15may

--- resources/views/pages/subpages/lawfirms_features.blade.php
+++ resources/views/pages/subpages/lawfirms_features.blade.php
@@ -15,7 +15,18 @@
         	</div>
         	{{-- content div --}}
         	<div class="col-sm-7 col-lg-7 col-md-7" >
-        		
+        		<p class="p-text">Adlaw offers a complete case management solution for lawyers, law firms and their clients. The features are stuffed with various sub-includes that empower you to accomplish more in lesser time. Here is a run-down of the sub-features – we encourage you to attempt our product for nothing but to audit its capacities altogether.</p>  
+
+                <h5 class="font-weight-bold text-captialize mt-2 mb-2">CREATE CASE</h5>
+                <p class="p-text">
+                    You can add any number of cases by court, case type, CNR number and year. With our software you can retrieve relevant information and auto-fills subtleties, for example, clients, opponents, opponent advocates, Honorable judges and court hall etc. 
+                    Adlaw Case Management Software - Cases List Everything you have to record, including a crate to include an essential portrayal of the case, is given. You can allocate each case to all or a portion of the team members that you have added to the software with the end goal of coordinated effort. You can also record date of documenting and include an essential depiction about the case, customer or any pertinent specifics. Once you have a list of cases you can sort and view by 'running' closed’ or ‘transferred/NOC’ filters.
+                </p>
+                <h5 class="font-weight-bold text-captialize mt-2 mb-2">SHOW CASES LIST</h5>
+                <p class="p-text">
+                    You can search cases by various criteria, for example, title, case number and more. There is no other quicker approach to discover and refer to cases, particularly in case you're working on various cases and have a full case diary. Here you can find All Cases, On Going Cases, Won Cases (Case Over in favor of client), Lost Cases (Case over against the favour) and Case that Withdrawn by client.
+                </p>
+                
         			{{-- <h1 class="font-weight-bold text-captialize mb-5">Consult Best Lawyer / Law Firms in India</h1>
         			<p class="p-text">
                             Seeing the demand of various Legal problems we allow you to hire the professional experts having good experience in Civil Law, Corporate Law, Start-up Solutions, Criminal Law, Consumer Law, Family Law and much more in all over India.
@@ -25,14 +36,27 @@
                      
                     </p>
  --}}
-                 <p class="p-text">
+                 {{-- <p class="p-text">
                      When it comes to businesses, a legal consultant is responsible for providing legal advice and assistance in handling disputes, analyzing and identifying the legal issues, drafting the legal documents, maintaining legal correspondence, etc. If any client is looking for legal advice or legal documentation, Adlaw has the lists of advisors from various states who can help you with just one click. Even our website is meant for those young and energized lawyers who are looking for cases. They have to register them-self on our website providing all documents so that if any clients want to hire you, you will get a notification. Adlaw offers free legal help and advice online for almost any legal matters by our top and best Lawyers in different verticals of Industry.       
-                </p>
+                </p> --}}
         	
         	</div>
         </div>
         <div class="row">
             <div class="col-md-10 m-auto">
+                <h5 class="font-weight-bold text-captialize mt-2 mb-2">SHOW CASES LIST</h5>
+                <p class="p-text">
+                    You can search cases by various criteria, for example, title, case number and more. There is no other quicker approach to discover and refer to cases, particularly in case you're working on various cases and have a full case diary. Here you can find All Cases, On Going Cases, Won Cases (Case Over in favor of client), Lost Cases (Case over against the favour) and Case that Withdrawn by client.
+                </p>
+                <h5 class="font-weight-bold text-captialize mt-2 mb-2">CASE RELATED DOCUMENT</h5>
+                <p class="p-text">
+                   You can also manage case related document. you can upload case related document on particular case wise.
+                </p>
+                 <h5 class="font-weight-bold text-captialize mt-2 mb-2">TRACK YOUR NOTES HISTORY</h5>
+                <p class="p-text">
+                   Adlaw provide add your important information on your Legal Cases as Notes in order to maintain the Case History.
+                </p>
+                <br>
                {{--  <p class="p-text">
                      When it comes to businesses, a legal consultant is responsible for providing legal advice and assistance in handling disputes, analyzing and identifying the legal issues, drafting the legal documents, maintaining legal correspondence, etc. If any client is looking for legal advice or legal documentation, Adlaw has the lists of advisors from various states who can help you with just one click. Even our website is meant for those young and energized lawyers who are looking for cases. They have to register them-self on our website providing all documents so that if any clients want to hire you, you will get a notification. Adlaw offers free legal help and advice online for almost any legal matters by our top and best Lawyers in different verticals of Industry.       
                 </p> --}}
